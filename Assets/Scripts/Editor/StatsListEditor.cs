fb2226bc478a8ec41671fde7ecd3fc4e2e30ebaf
Tyler Thompson <tjthompson@blizzard.com>
2015-01-02 21:17:13 -0800

    Initial set of Stats code - mostly editing and container

---
+++ Assets/Scripts/Editor/StatsListEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(Stat))]
+public class StatDrawer : PropertyDrawer {
+	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) 
+	{
+		Rect entryRect = position;
+		entryRect.xMax -= entryRect.width / 2;
+		EditorGUI.PropertyField(entryRect, property.FindPropertyRelative("m_Entry"), GUIContent.none);
+
+		Rect valueRect = position;
+		valueRect.xMin = entryRect.xMax;
+		EditorGUI.PropertyField(valueRect, property.FindPropertyRelative("m_Value"), GUIContent.none);
+	}
+}
+
+public class StatsListEditor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/StatsList.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Stat {
+	public STAT_ENTRY m_Entry;
+	public int m_Value;
+}
+
+public class StatsList : MonoBehaviour {
+
+	public List<Stat> m_StatsList;
+	public Dictionary<STAT_ENTRY, int> m_TestHashTable;
+
+	// Use this for initialization
+	void Start () {
+		m_StatsList.Sort(delegate(Stat x, Stat y) {
+			if (x.m_Entry > y.m_Entry) return 1;
+			if (x.m_Entry < y.m_Entry) return -1;
+			return 0;
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public enum STAT_ENTRY
+{
+	STAT_NONE = 0,
+	STAT_ATTACK,
+	STAT_LIFE,
+}
